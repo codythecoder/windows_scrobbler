a595f9f44642d0390ecb03edffebb1d70a9c2376
Unknown <cody.lovett.70@gmail.com>
2022-05-01 14:32:45 +1000

    Improved matching for songs and players

--- windows_scrobbler.py
+++ windows_scrobbler.py
@@ -1,13 +1,17 @@
 import asyncio
 import time
+import re
 
 from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 
-TARGET_ID = 'Microsoft.ZuneMusic'
-# Spotify.exe
+targets = {
+    'groove music': 'Microsoft.ZuneMusic$',
+    'spotify': '^Spotify.exe$',
+    'firefox': '^firefox.exe$',
+}
 
 # play time in senconds to scrobble
-play_time = 60 
+play_time = 60
 
 PLAYING_ID = 4
 
@@ -18,18 +22,24 @@ class PlayerNotFound(Exception):
 class Song_info(dict):
     """Basically a dict with some equivalence checks"""
     def __eq__(self, other: 'Song_info'):
-        if len(self.keys()) != len(other.keys()):
+        if not isinstance(other, dict):
+            # i mean I guess you should be able to compare it to a dict
             return False
-        for key in self:
+        # if len(self.keys()) != len(other.keys()):
+        #     return False
+
+        for key in set([*self.keys(), *other.keys()]):
+            # thumbnail changes each call so ignore it
             if key == 'thumbnail':
-                # thumbnail changes each time so ignore it
                 continue
-            if key not in other:
-                # print('not equal', key)
+            if key == 'scrobbled':
+                continue
+            # otherwise just make sure it's the same
+            if key not in self or key not in other:
                 return False
             if self[key] != other[key]:
-                # print('not equal', key)
                 return False
+
         return True
 
     def __ne__(self, other):
@@ -51,16 +61,16 @@ async def get_media_info() -> Song_info:
     current_session = sessions.get_current_session()
     # there needs to be a media session running
     if current_session and current_session.get_playback_info().playback_status == PLAYING_ID:
-        if current_session.source_app_user_model_id.startswith(TARGET_ID):
-            info = await current_session.try_get_media_properties_async()
+        info = await current_session.try_get_media_properties_async()
 
-            # song_attr[0] != '_' ignores system attributes
-            info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+        # song_attr[0] != '_' ignores system attributes
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
 
-            # converts winrt vector to list
-            info_dict['genres'] = list(info_dict['genres'])
+        # converts winrt vector to list
+        info_dict['genres'] = list(info_dict['genres'])
+        info_dict['player'] = current_session.source_app_user_model_id
 
-            return Song_info(info_dict)
+        return Song_info(info_dict)
 
     print(current_session.get_playback_info().playback_status)
     print(current_session.source_app_user_model_id)
@@ -70,36 +80,35 @@ async def get_media_info() -> Song_info:
     # See references for more information.
     raise PlayerNotFound('TARGET_PROGRAM is not the current media session')
 
+def any_search(patterns, s) -> bool:
+    return any(map(lambda x: re.search(x, s), patterns))
+
 
 if __name__ == '__main__':
     current_media_info = None
     while True:
         try:
-            new_media_info = asyncio.run(get_media_info())
-
-            start_time = time.time()
-            current_media_info = new_media_info
-            print('scrobbling', current_media_info['title'], 'by', current_media_info['artist'])
-            # check that we're still playing the same song
-            for i in range(play_time//2):
-                # I don't know how expensive get_media_info is, so we'll play it conservative
-                time.sleep(2)
-                new_media_info = asyncio.run(get_media_info())
-                if new_media_info != current_media_info:
-                    print('scrobble cancelled')
-                    # for key in new_media_info:
-                    #     if new_media_info[key] != current_media_info[key]:
-                    #         print(f'{repr(key)}:{repr(new_media_info[key])}')
-                    #         print(f'{repr(key)}:{repr(current_media_info[key])}')
-                    #         print()
-                    break
-            else:
-                print('scobbled')
-                # if we listened to it for the full time then wait until it's the next song
-                while new_media_info == current_media_info:
-                    time.sleep(2)
-                    new_media_info = asyncio.run(get_media_info())
-        except PlayerNotFound:
-            print('player not found (maybe not playing or other service playing simultaniously)')
+            # I don't know how expensive get_media_info is, so we'll play it conservative
             time.sleep(2)
+            new_media_info = asyncio.run(get_media_info())
+            if new_media_info is None:
+                continue
+            if not any_search(targets.values(), new_media_info['player']):
+                continue
+
+            if current_media_info != new_media_info:
+                start_time = time.time()
+                current_media_info = new_media_info
+                current_media_info['scrobbled'] = False
+                print('scrobbling', current_media_info['title'], 'by', current_media_info['artist'])
+            else:
+                # if we're still playing the same song
+                if time.time() > start_time + play_time:
+                    if not current_media_info['scrobbled']:
+                        print('scobbled')
+                        current_media_info['scrobbled'] = True
+
+        except PlayerNotFound:
+            pass
+            # print('player not found (maybe not playing or other service playing simultaniously)')
             
